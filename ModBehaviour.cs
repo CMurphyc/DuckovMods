a5e4ed79e37373bcf78fdb11dba4d4040b8fd36c
CMurphyc <cmurphyc22@gmail.com>
2025-11-06 02:10:19 +0800

    <feat>(Test) add test files that could be used in duckov

---
+++ ModBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using Duckov;
+using Duckov.Utilities;
+using UnityEngine.UI;
+using TMPro;
+namespace DuckovMods
+{
+    public class ModBehaviour : Duckov.Modding.ModBehaviour
+    {
+        private TextMeshProUGUI? loadMessage;
+        public TextMeshProUGUI LoadMessage
+        {
+            get
+            {
+                if (loadMessage == null)
+                {
+                    loadMessage = Instantiate(GameplayDataSettings.UIStyle.TemplateTextUGUI);
+                }
+                return loadMessage;
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.LeftAlt))
+            {
+                Debug.Log("Alt Key Down");
+                LoadMessage.SetText("Mod Has Loaded!");
+            }
+
+            if (Input.GetKeyUp(KeyCode.LeftAlt))
+            {
+                Debug.Log("Alt Key Up");
+                GameObject.Destroy(loadMessage);
+            }
+        }
+    }
+}
